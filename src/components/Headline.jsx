4a42f9a23586fa456fd689885be446f21b949e7d
Daniela <daniela.primacov@yahoo.com>
2025-05-01 20:38:01 +0200

    Added the Headline and improved the searching excuses technique

---
+++ src/components/Headline.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+import classes from "./Headline.module.css";
+
+const words = ["family", "work", "friends"];
+
+function Headline() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % words.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={classes.headline}>
+      <span>Make excuses for </span>
+      <span className={classes.wordWrapper}>
+        {words.map((word, i) => (
+          <span
+            key={i}
+            className={`${classes.word} ${
+              i === index ? classes.visible : classes.hidden
+            }`}
+          >
+            {word}
+          </span>
+        ))}
+      </span>
+      <span> here</span>
+      <p>Exit Strategies for Everyday Life</p>
+    </div>
+  );
+}
+
+export default Headline;
--- src/components/WelcomeBanner.jsx
+++ src/components/WelcomeBanner.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Headline from "./Headline";
 
 import classes from "./WelcomeBanner.module.css";
 
@@ -6,14 +7,7 @@ function WelcomeBanner() {
   return (
     <>
       <div className={classes.banner}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-          interdum molestie ante, at pharetra turpis lacinia sed. Aenean gravida
-          volutpat diam in commodo. Nam ut congue diam, a vehicula justo. Etiam
-          vulputate nisl eu elit rutrum maximus. Curabitur in lectus vel quam
-          sodales aliquet non faucibus turpis. Suspendisse eget lorem et metus
-          ornare dictum. !
-        </p>
+        <Headline />
         <div className={classes.actions}>
           <Link to="/about" className={classes["learn-btn"]}>
             Learn more
